636684b68f375dcd61adac4dc4c8b03fcd997a6d
k2core <7179062@gmail.com>
2022-08-01 18:16:46 +0900

    App.js에도 CSS Modules 적용
    Button.js에 이어서 확대 적용한다.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Button from "./Button";
+import styled from "./App.module.css";
 
 function App() {
   return (
     <div>
-      <h1>Welcome back!!!</h1>
+      <h1 className={styled.title}>Welcome back!!!</h1>
       <Button text={"Continue"} />
     </div>
   );
