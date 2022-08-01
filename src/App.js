877d3c240d99d5915796feaf03176a0797118d94
k2core <7179062@gmail.com>
2022-08-01 18:57:37 +0900

    소스 정리
    - useEffect를 배우기 전 소스 정리

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import Button from "./Button";
-import styled from "./App.module.css";
-
 function App() {
   return (
     <div>
-      <h1 className={styled.title}>Welcome back!!!</h1>
-      <Button text={"Continue"} />
+      <h1>Welcome back!!!</h1>
     </div>
   );
 }
