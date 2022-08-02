fed7d0469ae83e1161ff12ebc52309537b87d8b9
k2core <7179062@gmail.com>
2022-08-02 19:25:56 +0900

    버튼 클릭 시(상태가 변할 때), 매번 실행되는 예시
    state가 변할 때(버튼 클릭), 컴포넌트는 항상 다시 실행된다.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => prev + 1);
+  console.log("call an api");
   return (
     <div>
-      <h1>Welcome back!!!</h1>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
