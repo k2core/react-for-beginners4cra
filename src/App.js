b4dcc9ad55c1448a473d1232239e0180e6bbbca6
k2core <7179062@gmail.com>
2022-08-07 23:17:10 +0900

    proj.To Do List(3)
    toDos 배열 상태변수 사용 등

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import { useState } from "react";
 
 function App() {
   const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
   const onChange = (event) => setToDo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
     if (toDo === "") {
       return;
     }
+    setToDos((currentArray) => [toDo, ...currentArray]);
     setToDo("");
   };
+  console.log(toDos);
   return (
     <div>
+      <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input
           onChange={onChange}
