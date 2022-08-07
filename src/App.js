941acaba570ae4169537420f7a6660279ef2b82b
k2core <7179062@gmail.com>
2022-08-07 22:58:15 +0900

    proj.To Do List
    form 태그로 감싸고,
    button 태그 추가 등

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import { useState } from "react";
 function App() {
   const [toDo, setToDo] = useState("");
   const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+  };
   return (
     <div>
-      <input
-        onChange={onChange}
-        value={toDo}
-        type="text"
-        placeholder="Write your to do..."
-      />
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
