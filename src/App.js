75e80f053e4e1e14c3c75641bba98799651ecce5
k2core <7179062@gmail.com>
2022-08-11 09:55:36 +0900

    proj.Coin Tracker(2)
    가져온 데이터 담기

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers");
+    fetch("https://api.coinpaprika.com/v1/tickers").then((response) =>
+      response.json().then((json) => {
+        setCoins(json);
+        setLoading(false);
+      })
+    );
   }, []);
   return (
     <div>
