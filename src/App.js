d9874b03e5648b13753734197144b576b6204eaa
k2core <7179062@gmail.com>
2022-08-13 18:50:53 +0900

    proj.Movie App(6)
    리스트 출력(1)

--- src/App.js
+++ src/App.js
@@ -15,7 +15,19 @@ function App() {
   useEffect(() => {
     getMovies();
   }, []);
-  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>{movie.title}</div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
