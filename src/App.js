e98247545172d0f9fa0ee24f3a6f7e45205ffd36
k2core <7179062@gmail.com>
2022-08-13 15:35:02 +0900

    proj.Movie App(2)
    fetch로 데이터 확인

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+    )
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }, []);
   return <div>{loading ? <h1>Loading...</h1> : null}</div>;
 }
 
