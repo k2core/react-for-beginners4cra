2a78fd56f4dd542992a6ad087c06d616518702b9
k2core <7179062@gmail.com>
2022-08-13 17:04:13 +0900

    proj.Movie App(3)
    상태 변수 movies에 담기

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
     fetch(
       `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
     )
       .then((response) => response.json())
-      .then((json) => console.log(json));
+      .then((json) => {
+        setMovies(json.data.movies);
+        setLoading(false);
+      });
   }, []);
+  console.log(movies);
   return <div>{loading ? <h1>Loading...</h1> : null}</div>;
 }
 
